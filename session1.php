b3381231b126b7469009e52f682e88caef887ec9
Haris <haris120@gmail.com>
2024-07-26 10:54:36 +0500

    session1

---
+++ session1.php
@@ -0,0 +1,35 @@
+<?php
+
+//phpinfo();
+/*
+echo 'Hello World';
+echo '<h1>Hello World</h1>';
+echo ("Hello world from echo()");
+
+print('Hello World from Print()');
+*/
+
+$A = 10;
+$b = 5;
+
+// echo $A + $b;
+
+$myname = "Hamza";
+$mystatus = false;
+$iq = 0.5;
+$decide = null;
+// $decidenot;
+
+//echo $myName;
+//echo $myname;
+
+// check type
+// echo (gettype($decide));
+
+
+$name = "hamza";
+
+
+// echo $name . ' is a good boy, & his IQ level is :' . $iq;
+echo ($name . ' is a good boy, & his IQ level is :' . $iq);
+
