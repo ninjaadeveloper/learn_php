c7480d3eaa2db04a69361680984e34e115c88f67
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-07-31 10:48:03 +0500

    Day3

---
+++ session3.php
@@ -0,0 +1,52 @@
+<?php
+/*
+$name = "Hamza";
+echo $name;
+$name = "Affan";
+echo $name;
+*/
+
+define('a', 20.546456);
+define('name', 'Affan');
+// echo (name);
+// define('name', 'Affan');
+
+
+// print(Pi());
+
+$myvalue = min(200, 300, 500);
+$myvalue2 = max(200, 300, 500);
+
+
+// echo ($myvalue);
+// echo ($myvalue2);
+
+
+$myRandomValue = rand();
+// $myRandomValue1 = rand(1000,10000);
+// $myRandomValue1 = rand(1.5,50.465444654) / 20 * 82;
+
+// echo $myRandomValue;
+// echo $myRandomValue1;
+
+// echo __FILE__;
+// echo __DIR__;
+// echo __LINE__;
+
+
+function printName()
+{
+
+    echo 'Hello 11B <br>';
+}
+
+// printName();
+
+function printName1()
+{
+    return 'Hello 11B';
+}
+
+ $myVal =  printName1();
+
+ echo($myVal);
