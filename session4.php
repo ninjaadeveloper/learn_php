5aa8d8a57d13e28a3fb0238a7a715ab7f306dc36
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-08-02 09:56:07 +0500

    functions

---
+++ session4.php
@@ -0,0 +1,24 @@
+<?php
+
+function printName($namee)
+{
+    echo "Hello " . $namee . "<br>";
+}
+
+// printName('Affan');
+// printName('Zaki');
+
+
+function addTwoNumbers($num1, $num2)
+{
+    echo $num1 + $num2;
+}
+
+// addTwoNumbers(20, 50);
+
+function printOnlyString(string $namee)
+{ 
+    echo $namee;
+}
+printOnlyString('Affan');
+
