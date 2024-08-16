8c0ee6350fbdfb814c4abc7ee5227c8fad2402fe
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-08-16 10:10:33 +0500

    create

---
+++ crud/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_user";
+
+// $con = new mysqli($host, $username, $password, $database);
+$con = new mysqli('localhost', 'root', '', 'db_user');
+
+
+// if ($con) {
+//     echo "Database connected";
+// } else {
+//     echo "Database not connected";
+// }
